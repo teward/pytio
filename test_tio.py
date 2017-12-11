27f4b04c6517cb1005688694152e56f99db5c70b
Thomas Ward <teward@ubuntu.com>
2017-12-11 15:38:58 -0500

    Add a few new tests, Python2 specific.

--- test_tio.py
+++ test_tio.py
@@ -7,6 +7,28 @@ from pytio import Tio, TioRequest
 class TestTIOResults(unittest.TestCase):
     tio = Tio()
 
+    def test_valid_python2_request(self):
+        request = TioRequest(lang='python2', code="print \"Hello, World!\"")
+        response = self.tio.send(request)
+        self.assertIsNone(response.error)
+        if platform.python_version() >= '3.0':
+            self.assertIsInstance(response.result, str)
+        else:
+            # noinspection PyUnresolvedReferences
+            self.assertIsInstance(response.result, (unicode, str))
+        self.assertEqual(response.result.strip('\n'), "Hello, World!")
+
+    def test_invald_python2_request(self):
+        request = TioRequest(lang='python2', code="I'm a teapot!")
+        response = self.tio.send(request)
+        self.assertIsNone(response.result)
+        if platform.python_version() >= '3.0':
+            self.assertIsInstance(response.error, str)
+        else:
+            # noinspection PyUnresolvedReferences
+            self.assertIsInstance(response.error, (unicode, str))
+        self.assertIn('EOL while scanning string literal', response.error)
+
     def test_valid_python3_request(self):
         request = TioRequest(lang='python3', code="print('Hello, World!')")
         response = self.tio.send(request)
