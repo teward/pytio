d1f7af3785b366e15e770f259c0809e4590c17cb
Thomas Ward <teward@ubuntu.com>
2017-12-15 15:26:59 -0500

    Revert "Named tuples instead of TioFile and TioVariable classes; version bump."
    This reverts commit ad11678f4b51ad875d048f209bb6d47989228b35.

--- pytio/__init__.py
+++ pytio/__init__.py
@@ -1,20 +1,15 @@
 # coding=utf-8
 
-import collections
-
 from ._Tio import Tio
+from ._TioFile import TioFile
 from ._TioRequest import TioRequest
 from ._TioResponse import TioResponse
 from ._TioResult import TioResult
-
-# Some of the items previously defined as individual classes can instead be defined as named tuples.
-# So, use named tuples for those.
-TioFile = collections.namedtuple('TioFile', ['name', 'content'])
-TioVariable = collections.namedtuple('TioVariable', ['name', 'content'])
+from ._TioVariable import TioVariable
 
 __title__ = 'PyTIO'
 __author__ = 'Thomas Ward'
-__version__ = '0.1.1'
+__version__ = '0.1.0'
 __copyright__ = '2017 Thomas Ward'
 __license__ = 'AGPLv3+'
 __all__ = ('Tio', 'TioFile', 'TioRequest', 'TioResponse', 'TioResult', 'TioVariable')
